c95c9f74498f08a2fbefbee7df919f3231ddf08c
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-07-01 22:33:28 +0530

    Add cosine similarity calculation for claim embeddings

--- backend/agents/similarity_engine.py
+++ backend/agents/similarity_engine.py
@@ -1,10 +1,20 @@
 import pandas as pd 
 from sentence_transformers import SentenceTransformer
+import numpy as np
+from itertools import combinations
 
 def similarity_engine():
-    sentences = pd.read_csv("../../data/processed/unified_dataset.csv",usecols=['gemini_response'])
+    similarity_score = []
+    sentences = pd.read_csv("../../data/processed/unified_dataset.csv",usecols=['extracted_claim'])
     sentences = [i[0] for i in sentences.values]
     model = SentenceTransformer('all-MiniLM-L6-v2')    
     embedding = model.encode(sentences,normalize_embeddings=True)
-    print(embedding.shape)
+    comb = combinations(embedding,2)
+    for i in comb:
+        similarity_score.append(distance_of_vectors(i[0],i[1]))
+    print(similarity_score)
+
+def distance_of_vectors(vector1,vector2):
+    return np.dot(vector1,vector2)
+
 similarity_engine()
