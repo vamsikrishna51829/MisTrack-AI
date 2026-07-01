6ba728ac95f5f69cb3c5b23949ddd736006d98b9
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-07-01 18:48:08 +0530

    Encode claims into sentence embeddings

--- backend/agents/similarity_engine.py
+++ backend/agents/similarity_engine.py
@@ -2,14 +2,9 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 def similarity_engine():
-    sentence = [
-                "i love ai",
-                "i love machine learning",
-                "i love sun",
-                "redbull gives you wings",
-                "i can fly using redbull"
-                ]
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embedding = model.encode(sentence,normalize_embeddings=True)
+    sentences = pd.read_csv("../../data/processed/unified_dataset.csv",usecols=['gemini_response'])
+    sentences = [i[0] for i in sentences.values]
+    model = SentenceTransformer('all-MiniLM-L6-v2')    
+    embedding = model.encode(sentences,normalize_embeddings=True)
     print(embedding.shape)
 similarity_engine()
