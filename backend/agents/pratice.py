f940532848148a2c20cc69617f4843d68f2a45f9
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-07-23 22:55:45 +0530

    dataset building

---
+++ backend/agents/pratice.py
@@ -0,0 +1,10 @@
+import pandas as pd 
+from dataset_builder import news_combine
+
+# df = pd.read_csv("../../data/processed/politifact.csv")
+# news_combine(df1,df2)
+
+# df = df.drop(columns=["platform","source"])
+# df = df.assign(label = 0)
+# df.to_csv("../../data/processed/politifact.csv",index=False)
+
