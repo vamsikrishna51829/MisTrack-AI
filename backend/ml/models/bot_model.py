47aef800da5cfc42d01f1707b1da5aa0bd8c150a
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-08-13 13:16:26 +0530

    feat: build combined bot detection dataset

--- backend/ml/models/bot_model.py
+++ backend/ml/models/bot_model.py
@@ -1,2 +1,13 @@
 import pandas as pd 
 
+import pandas as pd 
+
+df1, df2 = map(pd.read_csv,["../../../data/raw/cresci-2017/genuine_accounts/users.csv","../../../data/raw/cresci-2017/social_spambots_1/users.csv"])
+
+df1 = df1.drop(columns=["test_set_1","test_set_2"])
+df2 = df2.drop(columns=["test_set_1"])
+df1["label"] = 0
+df2["label"] = 1
+df = pd.concat([df1,df2],ignore_index=True)
+
+df.to_csv("../../../data/processed/bot_dataset.csv",index=False)
