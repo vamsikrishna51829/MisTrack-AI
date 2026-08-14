2096d46dc62f5226ac152ac2784ef3aa933a5106
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-08-14 08:27:07 +0530

    feat: engineer behavioral features for bot detection

--- backend/ml/models/bot_model.py
+++ backend/ml/models/bot_model.py
@@ -2,8 +2,18 @@ import pandas as pd
 
 df = pd.read_csv("../../../data/processed/bot_dataset.csv")
 
-for i in df.columns:
-    if i not in ["label","statuses_count","followers_count","friends_count","created_at"]:
-        df = df.drop(columns=i)
+# df = df.drop(columns=["Unnamed: 0"])
+
+statuses_count = df["statuses_count"]
+account_age_days = df["account_age_days"]
+avg_daily_posts = []
+
+for i in range(len(df["statuses_count"])):
+    if account_age_days[i] != 0:
+        avg_daily_posts.append(statuses_count[i]/account_age_days[i])
+    else:
+        avg_daily_posts.append(0)
+
+df["avg_daily_posts"] = avg_daily_posts
 
 df.to_csv("../../../data/processed/bot_dataset.csv",index=False)
