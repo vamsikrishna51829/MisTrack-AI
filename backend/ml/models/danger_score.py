a6f742d4e4fed0ad8e9fb275913886ae64b0bd08
vamsikrishna51829 <vamsikrishna51829@gmail.com>
2026-08-28 21:16:06 +0530

    feat: generate synthetic danger score dataset

--- backend/ml/models/danger_score.py
+++ backend/ml/models/danger_score.py
@@ -1,31 +1,14 @@
 import random
 import pandas as pd
-from agents.claim_extractor import calling_gemini
-
-def generate_scenarios(count=10):
-        
-    scenarios = []
-
-    for _ in range(count):
-
-        scenario = {
-            "virality_prob": random.uniform(0, 1),
-            "bot_percentage": random.uniform(0, 1),
-            "mutation_severity": random.randint(0, 3),
-            "sentiment_extremity": random.uniform(0, 1),
-            "reach_score": random.uniform(0, 1)
-        }
-
-        scenarios.append(scenario)
-
-    return pd.DataFrame(scenarios)
-
 
 def main():
 
-    df = generate_scenarios()
-
-    df.to_csv("../../data/processed/danger_score.csv",index=False)
+    df = pd.read_csv("../../../data/processed/danger_score.csv")
+    print(df.shape)
+    danger_score = df['danger_score']
+    danger_score = [i*100 for i in danger_score]
+    df['danger_score'] = danger_score
+    df.to_csv("../../../data/processed/danger_score.csv",index=False)
 
 
 if __name__ == "__main__":
